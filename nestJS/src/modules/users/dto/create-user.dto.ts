eda6ddbf3d1117c8119cc36ab8912ae29446b5ff
AnhMinh <truonghoanganhminh2000@gmail.com>
2025-07-15 03:39:48 +0700

    reBuild

--- nestJS/src/modules/users/dto/create-user.dto.ts
+++ nestJS/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: 'name is not empty!' })
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail({}, { message: 'Invalid email format!' })
+  email: string;
+
+  @IsNotEmpty()
+  @IsNotEmpty({ message: 'password is required!' })
+  password: string;
+
+  phone: string;
+
+  address: string;
+
+  image: string;
+
+  role: string;
+}
